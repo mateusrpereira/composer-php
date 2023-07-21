c2578584564740cb87e821dfb880c14eca55a072
Mateus Ramos Pereira <mateusramospereira@hotmail.com>
2023-07-21 14:38:12 -0300

    Projeto buscador de cursos composer

---
+++ buscar-cursos.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+//Guzzle: realizar requisições HTTP
+use GuzzleHttp\Client;
+
+//DomCrawler: ler o HTML
+use Symfony\Component\DomCrawler\Crawler;
+
+//Guzzle
+$client = new Client();
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+$html = $resposta->getBody();
+
+//DomCrawler
+//$crawler = new Crawler();
+$crawler = new Crawler($html);
+
+//$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
